0d5338cebff6227e02668d5c3770c1f3ea3c040f
Abdulrazaq Ahmad Sabo <93339361+AbdulrazaqAS@users.noreply.github.com>
2025-05-04 11:08:55 +0000

    fragments added

---
+++ lens-peek/src/client.ts
@@ -0,0 +1,10 @@
+import { PublicClient, testnet } from "@lens-protocol/client";
+
+import { fragments } from "./fragments";
+
+export const client = PublicClient.create({
+  environment: testnet,
+  fragments,
+  //origin: "https://myappdomain.xyz",
+  //apiKey: "<SERVER-API-KEY>",  // to increase rate limit in server to server call
+});
