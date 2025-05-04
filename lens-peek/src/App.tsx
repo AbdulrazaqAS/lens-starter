9b4b211862a2fa82933311fed015e31ff6eeb5e8
Abdulrazaq Ahmad Sabo <93339361+AbdulrazaqAS@users.noreply.github.com>
2025-05-04 13:25:02 +0000

    connecting

--- lens-peek/src/App.tsx
+++ lens-peek/src/App.tsx
@@ -1,12 +1,45 @@
 import { Web3Provider } from "./Web3Provider";
 import { ConnectKitButton } from "connectkit";
+import { client } from "./client";
+import { signer } from "./signer";
+import { signMessageWith } from "@lens-protocol/client/viem";
+import { useEffect } from "react";
+
+const TESTNET_APP = "0xC75A89145d765c396fd75CbD16380Eb184Bd2ca7";
 
 const App = () => {
+  useEffect(() => {
+    async function setup() {
+      const authenticated = await getAuthentication();
+      if (authenticated.isErr()) {
+        console.error(authenticated.error);
+      }
+      console.log(authenticated);
+      // SessionClient: { ... }
+      const sessionClient = authenticated.value;
+      console.log(sessionClient);
+    }
+
+    setup();
+  }, []);
+
+  async function getAuthentication() {
+    const authenticated = await client.login({
+      onboardingUser: {
+        app: TESTNET_APP,
+        wallet: signer.address,
+      },
+      signMessage: signMessageWith(signer),
+    });
+
+    return authenticated;
+  }
+
   return (
     <Web3Provider>
       <ConnectKitButton />
     </Web3Provider>
-  )
+  );
 };
 
 export default App;
--- lens-peek/src/onboardingUser.ts
+++ lens-peek/src/onboardingUser.ts
@@ -2,17 +2,20 @@ import { client } from "./client";
 import { signer } from "./signer";
 import { signMessageWith } from "@lens-protocol/client/viem";
 
+const TESTNET_APP = "0xC75A89145d765c396fd75CbD16380Eb184Bd2ca7";
+
 const authenticated = await client.login({
   onboardingUser: {
-      app: "0x5678…",
-          wallet: signer.address,
-            },
-              signMessage: signMessageWith(signer),
-              });
+    app: TESTNET_APP,
+    wallet: signer.address,
+  },
+  signMessage: signMessageWith(signer),
+});
 
-              if (authenticated.isErr()) {
-                return console.error(authenticated.error);
-                }
-
-                // SessionClient: { ... }
-                const sessionClient = authenticated.value;
+if (authenticated.isErr()) {
+  console.error(authenticated.error);
+}
+console.log(authenticated);
+// SessionClient: { ... }
+const sessionClient = authenticated.value;
+console.log(sessionClient);
