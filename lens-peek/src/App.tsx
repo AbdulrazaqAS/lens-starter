66fed1a32f64bd10e3827047771c8cbcfa9957c4
Abdulrazaq Ahmad Sabo <93339361+AbdulrazaqAS@users.noreply.github.com>
2025-05-05 03:09:57 +0000

    fixing connection

--- lens-peek/src/App.tsx
+++ lens-peek/src/App.tsx
@@ -1,44 +1,17 @@
-import { Web3Provider } from "./Web3Provider";
 import { ConnectKitButton } from "connectkit";
-import { client } from "./client";
-import { signer } from "./signer";
-import { signMessageWith } from "@lens-protocol/client/viem";
-import { useEffect } from "react";
+import { useAccount, useWalletClient } from "wagmi";
 
 const TESTNET_APP = "0xC75A89145d765c396fd75CbD16380Eb184Bd2ca7";
 
 const App = () => {
-  useEffect(() => {
-    async function setup() {
-      const authenticated = await getAuthentication();
-      if (authenticated.isErr()) {
-        console.error(authenticated.error);
-      }
-      console.log(authenticated);
-      // SessionClient: { ... }
-      const sessionClient = authenticated.value;
-      console.log(sessionClient);
-    }
-
-    setup();
-  }, []);
-
-  async function getAuthentication() {
-    const authenticated = await client.login({
-      onboardingUser: {
-        app: TESTNET_APP,
-        wallet: signer.address,
-      },
-      signMessage: signMessageWith(signer),
-    });
-
-    return authenticated;
-  }
+  const account = useAccount();
 
   return (
-    <Web3Provider>
+    <div>
       <ConnectKitButton />
-    </Web3Provider>
+      {account.isDisconnected && <div>Not connected</div>}
+      {account.isConnected && <div>Connected Wallet: {account.address}</div>}
+    </div>
   );
 };
 
